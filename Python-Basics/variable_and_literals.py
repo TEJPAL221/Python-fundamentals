bddb93e4752ee829e278a24670c8c0877aacb579
TEJPAL221 <126184577+TEJPAL221@users.noreply.github.com>
2025-12-05 20:37:12 +0530

    Add variable assignments and literals examples
    Demonstrate variable assignment, value changes, and various literals in Python.

---
+++ Python-Basics/variable_and_literals.py
@@ -0,0 +1,60 @@
+#assigning values to variables.
+
+number=10
+name="Johnson"
+height=6.2
+
+print("Before changing values.")
+print(number)
+print(height)
+print(name)
+
+# changing the values of variables.
+number=98
+name="Harry"
+height=5.7
+
+print("After changing values.")
+print(number)
+print(height)
+print(name)
+
+# assigning multiple values to multiple variables.
+
+a, b, c=10, "yash", 7.7
+print(a)
+print(b)
+print(c)
+
+site1=site2="apple.com"
+print(site1)
+print(site2)
+
+#Some more literals in python
+
+is_true=True  #Boolean literal
+print(is_true)
+
+character='c'  #Character literal
+print(character)
+
+value=None     #null variable
+print(value)
+
+#Collection literals in python
+
+# list literal
+fruits = ["apple", "mango", "orange"] 
+print(fruits)
+
+# tuple literal
+numbers = (1, 2, 3) 
+print(numbers)
+
+# dictionary literal
+alphabets = {'a':'apple', 'b':'ball', 'c':'cat'} #key-value pair
+print(alphabets)
+
+# set literal
+vowels = {'a', 'e', 'i' , 'o', 'u'} 
+print(vowels)
