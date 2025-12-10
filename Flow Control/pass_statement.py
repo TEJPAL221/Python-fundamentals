7ad50c6bff60189186a8dee18fd87010730894d4
TEJPAL221 <126184577+TEJPAL221@users.noreply.github.com>
2025-12-10 21:28:04 +0530

    Create pass_statement.py

---
+++ Flow Control/pass_statement.py
@@ -0,0 +1,11 @@
+#Suppose we have a loop or a function that is not implemented yet, but we want to implement it in the future.
+# In such cases, we can use the pass statement.
+
+#example1
+
+n=10
+if n>=10:
+    pass        # we can add logic later
+print(10)
+
+# we can do same thing in function and classes
