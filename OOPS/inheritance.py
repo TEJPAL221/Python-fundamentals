4656853ffc4c828344baf85a4d2f7dbd9c6b8017
TEJPAL221 <126184577+TEJPAL221@users.noreply.github.com>
2025-12-13 19:19:57 +0530

    Implement examples of multiple and multilevel inheritance
    Added examples of multiple and multilevel inheritance in Python, including method resolution order.

--- OOPS/inheritance.py
+++ OOPS/inheritance.py
@@ -44,3 +44,60 @@ Hierarchical Inheritance: more than one child class are created from a single pa
 Hybrid Inheritance: combines more than one form of inheritance.
 '''
 
+# Multiple inheritance in python
+# A class can be derived from more than one superclass in python.
+
+class Mammal:
+    def mammal_info(self):
+        print("mammals can give direct birth")
+class WingedAnimal:
+    def winged_animal_info(self):
+        print("winged animal can flap")
+class Bat(Mammal,WingedAnimal):
+    pass
+b1=Bat()
+b1.mammal_info()
+b1.winged_animal_info()
+
+#Multilevel inheritance in python
+
+class SuperClass:
+
+    def super_method(self):
+        print("Super Class method called")
+
+class DerivedClass1(SuperClass):
+    def derived1_method(self):
+        print("Derived class 1 method called")
+
+class DerivedClass2(DerivedClass1):
+
+    def derived2_method(self):
+        print("Derived class 2 method called")
+
+
+d2 = DerivedClass2()
+
+d2.super_method()  # Output: "Super Class method called"
+
+d2.derived1_method()  # Output: "Derived class 1 method called"
+
+d2.derived2_method()  # Output: "Derived class 2 method called"
+
+# Method resolution order in python
+
+class SuperClass1:
+    def info(self):
+        print("Super Class 1 method called")
+
+class SuperClass2:
+    def info(self):
+        print("Super Class 2 method called")
+
+class Derived(SuperClass1, SuperClass2):
+    pass
+
+d1 = Derived()
+d1.info()  
+
+# Output: "Super Class 1 method called"
